9dae7c4adf4da684516e1306575138fd24cab6f0
sksamuel <sam@sksamuel.com>
2021-09-09 09:09:08 -0500

    Fixed duplicate test name

--- src/desktopMain/kotlin/bits.kt
+++ src/desktopMain/kotlin/bits.kt
@@ -1,4 +1,5 @@
 fun bitstring(bits: List<Boolean>): String {
+   require(bits.isNotEmpty())
    return buildString {
       repeat(bits.size) {
          if (bits[it]) append("1") else append("0")
--- src/desktopTest/kotlin/BitstringTest.kt
+++ src/desktopTest/kotlin/BitstringTest.kt
@@ -1,16 +1,18 @@
+import io.kotest.assertions.throwables.shouldThrowAny
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 
 class BitstringTest : DescribeSpec() {
    init {
       describe("bit strings") {
-         it("should set bits based on booleans") {
+         it("should set bits based on booleans 1") {
             bitstring(listOf(true, false, true, false, true)) shouldBe "10101"
             bitstring(listOf(false, false, false, false, false, false, false, true)) shouldBe "00000001"
          }
-         it("should set bits based on booleans") {
-            bitstring(listOf(true, false, true, false, true)) shouldBe "10101"
-            bitstring(listOf(false, false, false, false, false, false, false, true)) shouldBe "00000001"
+         it("should error on empty") {
+            shouldThrowAny {
+               bitstring(listOf())
+            }
          }
       }
    }
